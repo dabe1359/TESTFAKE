a22486548d59d2afed55fa23fde3c32ef00cd39a
Dabecassis <67354892+dabe1359@users.noreply.github.com>
2021-07-12 10:22:13 +0300

    added new galery

---
+++ TestGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestGit
+//
+//  Created by macintosh on 12/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
